1852c2c74c2f44213fa010a5967738b2f0c04feb
Aria Ren <r.sining@wustl.edu>
2020-05-02 00:22:41 -0700

    Update ml_pipeline_2.0.py
    Add more parameters for Grid Search

--- ml_pipeline_2.0.py
+++ ml_pipeline_2.0.py
@@ -84,9 +84,9 @@ def build_model():
         ])
     
     parameters = {
-    'text_pipeline__vect__ngram_range': [(1, 1)],
-    'clf__estimator__n_estimators': [10]
-    #'clf__estimator__min_samples_split':[2, 3],
+    'text_pipeline__vect__ngram_range': [(1, 1), (1, 2)],
+    'clf__estimator__n_estimators': [10, 20],
+    'clf__estimator__min_samples_split':[2, 3, 4]
     }
 
     cv = GridSearchCV(pipeline, param_grid=parameters, n_jobs=-1)
